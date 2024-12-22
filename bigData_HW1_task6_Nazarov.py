462df8d9f649e269f69f5135c8cbcd2fefe6b249
nazarovaaaa <79746096+nazarovaaaa@users.noreply.github.com>
2024-12-22 22:20:05 +0300

    Update bigData_HW1_task6_Nazarov.py

--- bigData_HW1_task6_Nazarov.py
+++ bigData_HW1_task6_Nazarov.py
@@ -1,122 +1,133 @@
-import math #для вычисления вспомогательных мат. функций
-import mmh3 # для вычисления хэша
-from scipy.integrate import quad #для вычисления интеграла в HyperLogLog (предварительно нужно загрузить scipy)
-import csv #для работы с csv-файлами
+#   Описание алгоритма:
+#   1.Прохожусь по файлу 1
+#   1.1.Подсчитываю в Counter точное число ключей (пока не превысили 1.000.000)
+#   1.2.Параллельно веду Count-Min Sketch (для подсчета примерной встречаемости)
+#   1.3.Также заведу BloomFilter (идею описал в п.2.3)
+#   2.Прохожусь по файлу 2
+#   2.1.Если в Counter1 < 1.000.000, веду Counter и для 2 файла
+#   2.2.Параллельно веду неточный подсчет, обращаясь в Count-Min Sketch из п.1.2
+#   2.3.Существует риск, когда из-за одинакового хэша ключи, которых не было в файле 1
+#   могут внести лишний вклад в подсчет - для этого можно предварительно "отсеивать"
+#   их с помощью BloomFilter из п.1.3
+#   По итогу понадобится ровно 1 проход по каждому из файлов
 
-class HyperLogLogModify:#реализацию модифицированный класс HyperLogLog для реализации merge
+#   Описание структур данных:
+#   1.Предлагаю в качестве размера BloomFilter взять N = 100.000.000 * 10,
+#   тогда оптимальное число хэш-функций будет k = N / n * ln(2)=10 * ln(2) ~ 7
+#   В таком случае вероятность ложного срабатывания по исходной формуле будет составлять
+#   P = (1 - (1 - 1 / N) ^ (k * n)) ^ k ~ 0.0082, что составляет 0.8 %.
+#   Чтобы учитывать все возможные уже реализованные оптимизации для ускорения работы,
+#   буду использовать BloomFilter из пакета bloom-filter (можно использовать из задания 2)
+#   2.В качестве размера Count-Min Sketch предлагаю взять w = [e/0.000001] ~ 2 718 281,
+#   а глубины d = [ln(1/0.01)] = 4, в таком случае оценка вхождений элемента
+#   с вероятностью как минимум 1-0.01 = 0.99, что составляет 99%, будет в промежутке
+#   от реального значения до реального значения * 1.000001
 
-  def __init__(self, b):
-    self.b = b
-    self.m = 1 << b
-    self.M = []
-    for i in range(self.m):
-      self.M.append(0)
+import numpy as np
+from collections import Counter
+import csv
+import mmh3
+from bloom_filter import BloomFilter
 
-  #для реализации merge
-  def __iter__(self):
-    return iter(self.M)
-  def __len__(self):
-    return self.m
-  def __setitem__(self, i, value):
-    self.M[i] = value
-  def __getitem__(self, i):
-    return self.M[i]
 
-  def put(self, s):
-    x = mmh3.hash(s, signed = False)
-    x = bin(x)[2:].zfill(32)
-    j = int(x[:self.b], 2)
-    w = x[self.b:]
-    r = 1
-    find_elem = False
-    for i in w:
-      if i == '1':
-        find_elem = True
-        break
-      r += 1
-    if find_elem == False:
-      r += 1
-    self.M[j] = max(self.M[j], r)
+def CountJoinSize(filename1, filename2, max_keys=1000000, max_size_join=10000000):
+    #   завожу Counter для точного подсчета числа вхождений различных ключей в файлы
+    c1, c2 = Counter(), Counter()
+    #   завожу структуру Min-Sketch (заполняю нулями)
+    d = 4
+    w = 2718281
+    Count_Min = np.zeros((d, w))
+    #   завожу BloomFilter из пакета (указываю размер и сразу рассчитанный процент ошибки)
+    Bloom = BloomFilter(max_elements=1000000000, error_rate=0.0082)
+    #   true, пока используем точный подсчет
+    accCalc = True
+    #   веду для подсчета
+    calc2 = 0  # неточного
+    calc1 = 0  # точного
+    #   обхожу 1ый файл
+    with open(filename1, 'r') as csvfile1:
+        csvreader1 = csv.reader(csvfile1)
+        for row in csvreader1:
+            #   заполняю Count-Min Sketch
+            for i in range(d):
+                pos = mmh3.hash(row[0], i) % w
+                Count_Min[i][pos] += 1
+            #   заполняю Bloom
+            Bloom.add(row[0])
+            #   пока использую точный подсчет - веду Counter
+            if accCalc is True:
+                if len(c1) >= max_keys:
+                    #   перехожу на неточный подсчет
+                    accCalc = False
+                    #   сразу могу очистить Counter
+                    c1.clear()
+                #   иначе добавляю ключ в Counter
+                elif row[0] not in c1:
+                    c1[row[0]] = 1
+                else:
+                    c1[row[0]] += 1
+    csvfile1.close()
 
-  def est_size(self):
-    z = 0.0
-    for j in range(self.m):
-      z += 2 ** (-self.M[j])
-    Z = z ** (-1)
-    if self.m < 128:
-      def integrand(x):
-        return math.log2((2+x)/(1+x)) ** self.m
-      alpha_m = (self.m * quad(integrand, 0, float('+inf'))[0]) ** (-1)
+    #   реализую проход по второму файлу
+    with open(filename2, 'r') as csvfile2:
+        csvreader2 = csv.reader(csvfile2)
+        for row in csvreader2:
+            #   если ключ не входит в блум-фильтр, значит такого ключа не было - пропускаем
+            if row[0] in Bloom:
+                temp_min_count = float('inf')
+                #   если не очищал Counter1, могу получить точное значение
+                if accCalc is True and row[0] in c1:
+                    calc2 += c1[row[0]]
+                #   иначе проверяю на вхождение в Count_min
+                else:
+                    for i in range(d):
+                        pos = mmh3.hash(row[0], i) % w
+                        if Count_Min[i][pos] < temp_min_count:
+                            temp_min_count = Count_Min[i][pos]
+                    #   накапливаю неточную оценку
+                    calc2 += temp_min_count
+
+        #   если еще используем точный подсчет, то веду Counter
+        if accCalc is True:
+            #   если вышли за 1.000.000, меняю флаг и очищаю
+            if len(c2) >= max_keys:
+                accCalc = False
+                c2.clear()
+            #   иначе добавляю в Counter
+            elif row[0] not in c2:
+                c2[row[0]] = 1
+            else:
+                c2[row[0]] += 1
+    csvfile2.close()
+
+    #   если используем точный подсчет, то работаю с Counter'ами
+    if accCalc is True:
+        #   если ключ присутствует в обоих, перемножаю значения
+        for key in c1:
+            if key in c2:
+                calc1 += c1[key] * c2[key]
+                #   если превысили 10.000.000, перехожу на неточный подсчет
+                if calc1 > max_size_join:
+                    accCalc = False
+                    break
+
+    if accCalc is True:
+        return calc1
     else:
-      alpha_m = 0.7213 / (1 + 1.079 / self.m)
-    E = alpha_m * self.m ** 2 * Z
-    if E <= 2.5 * self.m:
-      V = 0
-      for i in range(self.m):
-        if self.M[i] == 0:
-          V += 1
-      if V > 0:
-        E = self.m * math.log(self.m / V)
-    elif E > (1.0 / 30.0) * (1 << 32):
-      E = - (1 << 32) * math.log(1 - E / (2 << 32))
+        return calc2
 
-    return E
 
-def merge(HLL1, HLL2):#реализация оценки объединения множеств
-  max_len = max(len(HLL1), len(HLL2))
-  HLL = HyperLogLogModify(int(math.log2(max_len)))
-  for i in range(max_len):
-    hll1_reg_i = HLL1[i]
-    hll2_reg_i = HLL2[i]
-    HLL[i] = max(hll1_reg_i, hll2_reg_i)
+# протестировал на:
+# 1:
+#   gen_grouped_seq("hw1_task6_test2_csv1.csv", [(10, 100000)])
+#   gen_grouped_seq("hw1_task6_test2_csv2.csv", [(10, 100000)])
+#   print(CountJoinSize("hw1_task6_test2_csv1.csv", "hw1_task6_test2_csv2.csv", 100, 1000))
+# ожидаемо получил 0 (ключи между файлами уникальны)
+# 2:
+#   print(CountJoinSize("hw1_task6_test2_csv1.csv", "hw1_task6_test2_csv1.csv", 100, 1000))
+# ожидаемо получил 100.000 * 100.000 * 10 (10 групп по 100.000 одинаковых ключей между файлами)
+# 3:
+#   gen_grouped_seq("hw1_task6_test3_csv1.csv", [(1000000, 1)])
+#   print(CountJoinSize("hw1_task6_test3_csv1.csv", "hw1_task6_test3_csv1.csv", 100, 1000))
+# свалился на неточный расчет и получил 1008871 вместо 1.000.000 (по одной паре совпадающих ключей)
 
-  return HLL
-
-def CountJoinSize(filename1, filename2, max_keys = 1000000, max_size_join = 10000000):
-  c1, c2 = set(), set()
-  #true, если используем точный подсчет
-  accCalc = True
-  HLL1, HLL2 = HyperLogLogModify(10), HyperLogLogModify(10)
-  len1, len2 = 0, 0
-  #реализую проход по первому файлу
-  with open(filename1, 'r') as csvfile1:
-    csvreader1 = csv.reader(csvfile1)
-    for row in csvreader1:
-      HLL1.put(row[0])
-      if accCalc == True:
-        #если число ключей добралось до миллиона, заканчиваем точный подсчет
-        if len(c1) >= max_keys:
-          #переходим на оценку
-          accCalc = False
-          #очищаем множество
-          c1.clear()
-        #если текущей ключ уникален, то добавляю в set
-        elif row[0] not in c1:
-          c1.add(row[0])
-  csvfile1.close()
-  #реализую проход по второму файлу
-  with open(filename2, 'r') as csvfile2:
-    csvreader2 = csv.reader(csvfile2)
-    for row in csvreader2:
-      HLL2.put(row[0])
-      if accCalc == True:
-        if len(c2) >= max_keys:
-          accCalc = False
-          c2.clear()
-        elif row[0] not in c2:
-          c2.add(row[0])
-  csvfile2.close()
-  answer = 0
-  if accCalc == True:
-    answer = len(c1 & c2)
-    return answer
-  #если до сюда дошли, значит - точный подсчет не нужен
-  #выполняю merge и подсчитываю с помощью операций с множествами
-  #в случае, когда для первого файла заполнены set, буду использовать точное число вместо HLL
-  P1, P2 = 0, 0
-  if (len(c1) < max_keys and len(c1) > 0):
-    P1 = len(c1)
-  else:
-    P1 = HLL1.est_size()
-  answer = P1 + HLL2.est_size() - merge(HLL1, HLL2).est_size()
-  return answer
